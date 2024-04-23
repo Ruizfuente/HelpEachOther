5f2a4b580c63e0254708b3d3311505d1b9daeff1
MiguelMartinezMateos <47322591+MiguelMartinezMateos@users.noreply.github.com>
2024-04-23 23:43:34 +0200

    main page creation with styling and information

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlaceTypes } from '../../resources/enums/placeTypes.enum';
+import { PlacesService } from '../../services/places/places.service';
+import { UsuariosService } from '../../services/places/usuarios.service';
+import { User } from '../../models/user.model';
+import { Place } from '../../models/place.model';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
 
+  public _placeTypes = PlaceTypes;
+  public placesToShow: Place[] = [];
+
+  constructor(
+    private placesService: PlacesService,
+    private usuariosService: UsuariosService
+  ) {
+    this.placesToShow = this.placesService.getAllValidPlaces(5, 40.4177, -3.7042).slice(0, 18);
+  }
+
+
+  getUser(id: number): User {
+    return this.usuariosService.getUserById(id);
+  };
 }
