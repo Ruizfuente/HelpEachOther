7b8a965e67e3e48c5249d4bccad3a6d9461c99a8
MiguelMartinezMateos <47322591+MiguelMartinezMateos@users.noreply.github.com>
2024-04-27 14:33:53 +0200

    mocked data near user location

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,20 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-// import { Map, tileLayer } from 'leaflet';
 import * as L from 'leaflet';
 import { PlacesService } from '../../services/places/places.service';
 import { Place } from '../../models/place.model';
 import { NgxLeafletLocateModule } from '@runette/ngx-leaflet-locate';
-// import {MatIconModule} from '@angular/material/icon';
-
-// import { latLng, Map, Control, LocationEvent } from 'leaflet';
 import { UsuariosService } from '../../services/places/usuarios.service';
 import { User } from '../../models/user.model';
-
-
-// siguientes pasos:
-// 1. crear estructura de modulos YA NO HAYYYY
-// 2. hacer: https://github.com/runette/ngx-leaflet-locate
 
 @Component({
   selector: 'app-map',
@@ -28,27 +19,16 @@ export class MapComponent {
   public placesToShow: Place[] = [];
   public map: L.Map;
   public marker: any;
+  public firstTimeAux: true;
   public circle: any;
   public zoomed: any;
-
-  // public locateOptions: L.Control.LocateOptions = {
-  //   flyTo: true,
-  //   keepCurrentZoomLevel: true,
-  //   locateOptions: {
-  //     enableHighAccuracy: true,
-  //   },
-  //   // icon: 'material-icons md-18 target icon',
-  //   icon: 'bi bi-geo-fill',
-  //   clickBehavior: {
-  //     inView: 'stop',
-  //     outOfView: 'setView',
-  //     inViewNotFollowing: 'setView'
-  //   }
-  // };
+  public auxNumbersToMockData = [[-0.002, 0.005], [-0.001, -0.005], [-0.004, 0.001], [0.002, -0.0015], [0.008, 0.005], [0.0074, 0.003]];
+  public startPosition: number[] = [40.4170547, -3.7033967];
 
   constructor(
     private placesService: PlacesService,
-    private usuariosService: UsuariosService
+    private usuariosService: UsuariosService,
+    // private cacheService: CacheService
   ) {
   }
 
@@ -64,13 +44,10 @@ export class MapComponent {
   }
 
   toggleLocation(map: L.Map) {
-    // watchPossition esta pendiente de cambios, getCurrentPosition lo hace solo una vez
 
     let marker = this.marker;
-    // let circle = this.circle;
     let zoomed = this.zoomed;
-    // siguiente paso: USAR https://stackoverflow.com/questions/39253172/navigator-geolocation-watchposition-return-position-unavailable
-    // EN LUGAR DE WATCHPOSSITION O BUSCAR UNA ALTERNATIVA MAS PROPIA PARA ANGULAR PUDIENDO USAR THIS
+
     navigator.geolocation.watchPosition(onLocationSuccess, onLocationError);
 
     function onLocationSuccess(pos: any) {
@@ -80,15 +57,10 @@ export class MapComponent {
 
       if (marker) {
         map.removeLayer(marker);
-        // map.removeLayer(circle);
       }
       marker = L.marker([lat, lng]).addTo(map);
-      // circle = L.circle([lat, lng], {radius: accuracy}).addTo(map);
-
-      if (!zoomed) {
-        // zoomed = map.fitBounds(circle.getBounds());
-      }
       map.setView([lat, lng]);
+      // location.reload();
     }
 
     function onLocationError(err: any) {
@@ -99,45 +71,14 @@ export class MapComponent {
     }
   }
 
-
-  // onMapReady(map: L.Map) {
-  //   // this.map = map;
-  //   console.log("the map is redy")
-  // }
-
-  // onNewLocation(e: L.LocationEvent) {
-  //   console.log("New location", e)
-  // }
-
-  // $('#actions').find('a').on('click', function() {
-  //    locateUser();
-  // });
-  // choice_locations(): GEojsonobje{
-  //   console.log("choiced")
-  // }
   createMap() {
-    this.map = new L.Map('map').setView([40.4170547, -3.7033967], 16);
+    this.map = new L.Map('map').setView([this.startPosition[0], this.startPosition[1]], 15);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { // PATH WITH MORE LAYERS: https://leaflet-extras.github.io/leaflet-providers/preview/
       maxZoom: 19,
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-
-    // L.control.locate().addTo(map);
-
-
-    // var searchControl = new L.Control.Search({
-    //   layer: countriesJS,
-    //   propertyName: 'NAME',
-    //   circleLocation: false
-    // });
-
-    // searchControl.on('search_locationfound', function (e) {
-    //   e.layer.setStyle({ fillColor: '#3f0', color: '#0f0' });
-    // })
-
-    // map.addControl(searchControl);
   }
 
   getPlacesToShow() {
@@ -145,13 +86,32 @@ export class MapComponent {
     this.placesToShow = this.placesService.getAllValidPlaces(5, 40.4177, -3.7042);
   }
 
+  mockNearPlaces() {
+    debugger
+    setTimeout(() => {
+      let center = this.map.getCenter();
+      for (let i = 0; i < this.auxNumbersToMockData.length - 1; i++) {
+        let point = this.placesToShow[i];
+        point.coordinates.latitude = center.lat + this.auxNumbersToMockData[i][0];
+        point.coordinates.longitude = center.lng + this.auxNumbersToMockData[i][1];
+      }
+      this.drawPlacesListInMap(this.placesToShow.slice(0, this.auxNumbersToMockData.length - 1));
+    }, 500);
+  }
+
   drawPlacesToShow() {
     this.getPlacesToShow();
+    this.mockNearPlaces();
+
     // Siguiendo: https://leafletjs.com/examples/custom-icons/
+    this.drawPlacesListInMap(this.placesToShow);
+  }
+
+  drawPlacesListInMap(placesToShow: Place[]){
     let iconWidth = 60;
     let map = this.map;
 
-    this.placesToShow.forEach(place => {
+    placesToShow.forEach(place => {
       let curr_icon = L.icon({
         iconUrl: `assets/icons/map-pin/${place.type}.png`,
         iconSize: [iconWidth, iconWidth],
@@ -168,24 +128,11 @@ export class MapComponent {
         // let possition = myPositionMarker.getLatLng();
         map.setView([place.coordinates.latitude, place.coordinates.longitude])
 
-        const elemento_to_scroll = document.querySelector("#scroll-to-"+place.id)
-        if(elemento_to_scroll){
+        const elemento_to_scroll = document.querySelector("#scroll-to-" + place.id)
+        if (elemento_to_scroll) {
           elemento_to_scroll.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
         }
       });
-
-      //   myPositionMarker.on("click", function() {
-      //     var pos = map.latLngToLayerPoint(myPositionMarker.getLatLng());
-      //     pos.y -= 25;
-      //     var fx = new L.PosAnimation();
-
-      //     fx.once('end',function() {
-      //         pos.y += 25;
-      //         fx.run(myPositionMarker._icon, pos, 0.8);
-      //     });
-
-      //     fx.run(myPositionMarker._icon, pos, 0.3);
-      // });
 
     });
   }
